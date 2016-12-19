a7b5196ee28bb34ac1bd3455a3acf2d9ff40a254
Fabian Orccon <fabian.orccon@pucp.pe>
2016-12-19 20:18:19 +0000

    tracker: first idea

--- src/tracker.py
+++ src/tracker.py
@@ -1,6 +1,7 @@
 import sys
 import numpy as np
 import cv2
+from IPython import embed
 
 
 class Tracker:
@@ -8,15 +9,25 @@ class Tracker:
     def __init__(self):
         self.cap = cv2.VideoCapture(0)
         self.tracking = False
-        self.ROIs = []
+        self.ROIs_pts = []
+        # self.ROIs_hsv = []
+        self.ROIs_hist = []
+        self.termination =\
+            (cv2.TERM_CRITERIA_EPS | cv2.TERM_CRITERIA_COUNT, 10, 1)
 
     def updateROIs(self, frame):
         face_cascade = cv2.CascadeClassifier(self.CASCADE_FILENAME)
         faces = face_cascade.detectMultiScale(frame, scaleFactor=1.1,
             minNeighbors=5, minSize=(120,120))
         for (x, y, w, h) in faces:
-           self.ROIs.append((x, y, w, h))
-        self.tracking = bool(self.ROIs)
+            roi = frame[y : y + h, x : x + w]
+            roi_hsv = cv2.cvtColor(roi, cv2.COLOR_BGR2HSV)
+            roi_hist = cv2.calcHist([roi_hsv], [0], None, [180], [0, 180])
+            roi_hist = cv2.normalize(roi_hist, roi_hist, 0, 255, cv2.NORM_MINMAX)
+            self.ROIs_hist.append(roi_hist)
+            self.ROIs_pts.append((x, y, w, h))
+            # self.ROIs.append((x, y, w, h))
+        self.tracking = bool(self.ROIs_pts)
 
     def run(self):
         while True:
@@ -28,9 +39,22 @@ class Tracker:
             if not self.tracking:
                 self.updateROIs(frame)
             else:
-                for x, y, w, h in self.ROIs:
-                    color = (0, 255, 0)
-                    cv2.rectangle(frame, (x, y), (x + w, y + h), color, 2)
+                for i in range(len(self.ROIs_pts)):
+                    roi_pts = self.ROIs_pts[i]
+                    roi_hist = self.ROIs_hist[i]
+                    hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+                    back_proj = cv2.calcBackProject([hsv], [0], roi_hist,
+                        [0, 180], 1)
+
+                    print(self.ROIs_pts)
+                    ret, self.ROIs_pts[i] = cv2.CamShift(back_proj,
+                        self.ROIs_pts[i], self.termination)
+                    print(self.ROIs_pts)
+
+                    pts = cv2.boxPoints(ret)
+                    pts = np.int0(pts)
+                    
+                    box = cv2.polylines(frame, [pts], True, (0, 255, 0), 2)
 
             cv2.imshow("CAM", frame)
             if cv2.waitKey(1) & 0XFF == ord('q'):
